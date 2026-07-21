a104a0d71ccd979975e4cb0845e5debc15d002fc
세은 <ase59199613@gmail.com>
2026-07-21 18:42:38 +0900

    Update caseStudies.ts

--- src/data/caseStudies.ts
+++ src/data/caseStudies.ts
@@ -138,6 +138,7 @@ export const caseStudies: CaseStudy[] = [
       { label: "LEVEL DESIGN", image: "/images/unhinged-leveldesign.png" },
       { label: "MAP", image: "/images/unhinged-map.png" },
       { label: "DIARY", image: "/images/unhinged-diary.png" },
+      { label: "GOOGLE SHEET", image: "/pdf/ncai.pdf" },
       { label: "REWARD", image: "/images/Reward.jpg" },
     ],
   },
